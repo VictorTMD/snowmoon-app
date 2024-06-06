a8cc41151eea81414620ec29b6a7f6b2cef44324
Victor Morales <victor_teo_17@hotmail.com>
2024-06-06 23:06:00 +0200

    creacion del header

---
+++ src/components/Header.jsx
@@ -0,0 +1,104 @@
+'use client';
+import { useState } from 'react';
+import Image from 'next/image';
+import snoWMoon from '../../public/snoWMoon.svg';
+import Link from 'next/link';
+
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <section className='flex w-full justify-center'>
+      <header className='w-full m-3 pr-5 rounded-xl h-24 bg-color-head flex items-center justify-between 850pxutil:w-screen 850pxutil:m-0 850pxutil:rounded-none'>
+        <Image
+          className='h-25 w-48 ml-2'
+          src={snoWMoon}
+          alt='logo de snowmoon'
+        ></Image>
+
+        <nav
+          className={`text-text-color  fixed top-0  left-0 w-full  h-64 flex flex-col justify-center items-center gap-4 text-xl font-medium 850px:static 850px:flex-row 850px:justify-end   850px:h-auto  ${
+            isOpen ? 'flex' : 'hidden'
+          } 850px:flex 850pxutil:mt-24 rounded-b-lg 850pxutil:bg-color-head  `}
+        >
+          <Link
+            className='hover:text-second-color 850pxutil:hover:text-color-white link 850pxutil:w-full 850pxutil:hover:bg-text-color  850pxutil:h-11 text-center pt-1.5 transition duration-1000 ease-out'
+            href='/'
+            onClick={() => setIsOpen(false)}
+          >
+            INICIO
+          </Link>
+          <Link
+            className='hover:text-second-color  850pxutil:hover:text-color-white link 850pxutil:w-full 850pxutil:hover:bg-text-color  850pxutil:h-11  text-center pt-1.5
+            transition duration-1000 ease-out'
+            href='/catalogo'
+            onClick={() => setIsOpen(false)}
+          >
+            CATALOGO
+          </Link>
+          <Link
+            className='hover:text-second-color  850pxutil:hover:text-color-white link 850pxutil:w-full 850pxutil:hover:bg-text-color  850pxutil:h-11  text-center pt-1.5
+            transition duration-1000 ease-out'
+            href='/acerca'
+            onClick={() => setIsOpen(false)}
+          >
+            ACERCA
+          </Link>
+          <Link
+            className='hover:text-second-color  850pxutil:hover:text-color-white link 850pxutil:w-full 850pxutil:hover:bg-text-color  850pxutil:h-11  text-center pt-1.5
+            transition duration-1000 ease-out'
+            href='/cuidados'
+            onClick={() => setIsOpen(false)}
+          >
+            CUIDADOS
+          </Link>
+          <Link
+            className='hover:text-second-color  850pxutil:hover:text-color-white link 850pxutil:w-full 850pxutil:hover:bg-text-color  850pxutil:h-11  text-center pt-1.5
+            transition duration-1000 ease-out'
+            href='/contacto'
+            onClick={() => setIsOpen(false)}
+          >
+            CONTACTO
+          </Link>
+        </nav>
+
+        <div
+          className='ml-6 850px:hidden flex text-text-color'
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? (
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='size-6 cursor-pointer'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M6 18 18 6M6 6l12 12'
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='size-6 cursor-pointer'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5'
+              />
+            </svg>
+          )}
+        </div>
+      </header>
+    </section>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,31 +6,21 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      'text-color': '#341065',
-      'second-color': '#893AED',
-      'color-head': '#e0d6fecc',
-      'color-sections': '#e0d6fe40',
-      'color-cards': '#e0d6fe',
-    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'text-color': '#341065',
+        'color-white': '#ffff',
+        'second-color': '#893AED',
+        'color-head': '#e0d6fecc',
+        'color-sections': '#e0d6fe40',
+        'color-cards': '#e0d6fe',
+        'color-red': '#0000',
       },
       screens: {
         '850px': '850px',
         '850pxutil': { max: '850px' },
       },
     },
-    variants: {
-      extend: {
-        textColor: ['hover'],
-        backgroundColor: ['hover'], // Asegúrate de que hover:text-color esté habilitado
-      },
-    },
   },
-
   plugins: [],
 };
