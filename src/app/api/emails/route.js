d93af568f39b23a6fee4e0ad59b4fbbf60c226a7
Victor <victor_teo_17@hotmail.com>
2024-12-05 16:36:43 +0100

    agrego api de envio de email

---
+++ src/app/api/emails/route.js
@@ -0,0 +1,54 @@
+import nodemailer from 'nodemailer';
+
+export async function POST(req) {
+  try {
+    const body = await req.json(); // Parsear el cuerpo de la solicitud
+    const { name, email, message } = body;
+
+    // Configuración del transporte SMTP con Hostalia
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.servidor-correo.net', // Reemplaza con tu dominio de Hostalia
+      port: 587, // Usualmente es 587 para conexiones seguras o 465 si usas SSL
+      // secure: true, // Cambia a 'true' si usas SSL
+      auth: {
+        user: 'info@snowmoonartesania.com', // Tu correo de Hostalia
+        pass: 'CopitoLunita24@', // La contraseña de tu correo
+      },
+    });
+
+    // Opciones para el correo
+    const mailOptions = {
+      from: email, // Correo del usuario que envía el mensaje
+      to: 'info@snowmoonartesania.com', // Tu correo de Hostalia
+      subject: 'Nuevo mensaje de contacto',
+      text: `Nombre: ${name}\nCorreo: ${email}\nMensaje:\n${message}`, // Cuerpo del mensaje
+    };
+
+    // Enviar el correo
+    await transporter.sendMail(mailOptions);
+
+    // Respuesta de éxito
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: 'Correo enviado correctamente',
+      }),
+      {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  } catch (error) {
+    console.error('Error al enviar el correo:', error);
+    return new Response(
+      JSON.stringify({
+        success: false,
+        message: 'Hubo un error al enviar el correo',
+      }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+}
--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import { FiGift } from 'react-icons/fi';
 import { RiCandleLine } from 'react-icons/ri';
 import { pompiere } from '@/libs/font';
 
 export default function Contacto() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [status, setStatus] = useState('');
+
+  const handleChangue = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus('Enviando...');
+
+    const response = await fetch('/api/emails', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setTimeout(() => {
+        setStatus('Mensaje enviado con éxito');
+      }, 5000);
+      setFormData('');
+    } else {
+      setStatus('Hubo un error al enviar el mensaje');
+    }
+  };
+
   return (
     <section id='contacto' className='h-screen min-h-screen w-full px-4 pt-20'>
       <div className='flex justify-center items-center gap-2 mt-8'>
@@ -15,7 +54,64 @@ export default function Contacto() {
         </p>
         <FiGift className='text-2xl text-second-color ' />
       </div>
-      <div className='w-full h-full rounded-md bg-color-sections mt-5'></div>
+      <div className='w-full h-full rounded-md bg-color-sections mt-5'>
+        <form onSubmit={handleSubmit} className='p-4 space-y-4'>
+          <div>
+            <label htmlFor='name' className='block text-lg'>
+              Nombre:
+            </label>
+            <input
+              type='text'
+              id='name'
+              name='name'
+              className='w-full p-2 border'
+              value={formData.name}
+              onChange={handleChangue}
+              required
+              placeholder='Ingresa tu nombre *'
+            />
+          </div>
+          <div>
+            <label htmlFor='email' className='block text-lg'>
+              Correo Electrónico
+            </label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              className='w-full p-2 border'
+              value={formData.email}
+              onChange={handleChangue}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor='message' className='block text-lg'>
+              Mensaje
+            </label>
+            <textarea
+              id='message'
+              name='message'
+              className='w-full p-2 border'
+              rows='4'
+              value={formData.message}
+              onChange={handleChangue}
+              required
+            />
+          </div>
+          <div className='w-full flex justify-center'>
+            <button
+              type='submit'
+              className='w-48  bg-color-butoon border-2 border-second-color hover:bg-second-color  hover:border-text-color'
+            >
+              Enviar
+            </button>
+          </div>
+        </form>
+        {status && (
+          <p className='mt-4 w-24 text-center bg-color-butoon'>{status}</p>
+        )}
+      </div>
     </section>
   );
 }
